99f1e661ecd95c9c4ec2d3548cb66947352f4d24
Dipen <dipendraboyaju222@gmail.com>
2025-06-09 11:19:22 +0545

    ch

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app = FastAPI()
 # Allow frontend origin
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://prateekinnovations.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
